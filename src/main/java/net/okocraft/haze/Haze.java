4b9c9b59cf575303b409dcd5b351b5e61916b043
akaregi <akg.tachibana@gmail.com>
2019-03-07 18:39:47 +0900

    Added boilerplate files

---
+++ src/main/java/net/okocraft/haze/Haze.java
@@ -0,0 +1,43 @@
+/*
+ * This file is a part of Haze.
+ *
+ * Haze, Player's Point Manager.
+ * Copyright (C) 2019 OKOCRAFT
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.okocraft.haze;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author OKOCRAFT
+ */
+public class Haze extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
+}
